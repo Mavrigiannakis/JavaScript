dba5be55ce5da35ff0e3f3bee5b1cecd1404b17b
stelios <stylianos.mavrigiannakis@yahoo.com>
2022-09-12 10:40:13 +0300

    practice js in school

---
+++ JS-tiedostot/javascript-perusteet.js
@@ -0,0 +1,22 @@
+function laskut()
+{
+  var luku1, luku2 , summa, erotus, osamaara, tulo;
+  luku1 =10;
+  luku2 =2;
+  summa = luku1 + luku2;
+  erotus = luku1 - luku2;
+  osamaara = luku1 / luku2;
+  tulo = luku1 * luku2;
+  console.log("Summa = " + summa);
+  console.log("Erotus = " + erotus);
+  console.log("Osamäärä = " + osamaara);
+  console.log("Tulo = " + tulo);
+}
+function liitaSanat()
+{
+  var t1, t2, t3;
+  t1 = "Stelios ";
+  t2 = "on ";
+  t3 = "hyvä opiskelija";
+  console.log(t1 + t2 +t3)
+}
