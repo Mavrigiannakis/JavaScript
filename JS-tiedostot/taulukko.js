9021a18ded94bc0aa7499e28dc5a501f1eb7a307
stelios <stylianos.mavrigiannakis@yahoo.com>
2022-09-14 10:22:49 +0300

    ok

---
+++ JS-tiedostot/taulukko.js
@@ -0,0 +1,18 @@
+function esimerkkiTaulukko()
+{
+  var autot = [];
+  var caarat = ["Tesla", "VWi3", "Hyundai ionic"];
+  autot.push("Jeep");
+  document.write(caarat[1] + " " +autot[0] + "<br />");
+  document.write("Caara = " + caarat + "<br />");
+  document.write("Autot = " + autot + "<br />");
+  var siirettava = caarat.pop();
+  document.write("Siirettävä = " + siirettava);
+  document.write("<br />Caarat taulukko = " + caarat);
+  autot.unshift(siirettava)
+  document.write("<br />Autot taulukko = " + autot);
+  caarat.push() = autot.pop();
+  document.write("<br />Caarat taulukko = " + caarat + "<br />Autot taulukko = " + autot);
+
+
+}
