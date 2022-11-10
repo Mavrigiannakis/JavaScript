678a4197e95f2c155454b3c9fb2845dcc9050c4d
Mavrigiannakis <stylianos.mavrigiannakis@yahoo.com>
2022-11-10 15:25:16 +0200

    api

--- JavaScript-main/API/fetch.js
+++ JavaScript-main/API/fetch.js
@@ -7,13 +7,13 @@ function tietoa()
 
 
 
-    document.getElementById('tietoa').innerHTML = "Name:" + data.results[0].name.title + " " + data.results[0].name.first + " " + data.results[0].name.last
-     + "<br>" + "Adress:" + data.results[0].location.street.number + " " + data.results[0].location.street.name
+    document.getElementById('tietoa').innerHTML = "<b>Name: </b>" + data.results[0].name.title + " " + data.results[0].name.first + " " + data.results[0].name.last
+     + "<br>" + "<b>Adress: </b>" + data.results[0].location.street.number + " " + data.results[0].location.street.name
      + "<br>" + data.results[0].location.city + " " + data.results[0].location.state
      + "<br>" + data.results[0].location.postcode + " " + data.results[0].location.country
-     + "<br>" + "Phone Number:" + data.results[0].phone
-     + "<br>" + "Email:" + data.results[0].email
-     + "<br>" +  "Login + Password:" + data.results[0].login.username + " " + data.results[0].login.password;
+     + "<br>" + "<b>Phone Number: </b>" + data.results[0].phone
+     + "<br>" + "<b>Email: </b>" + data.results[0].email
+     + "<br>" +  "<b>Login + Password: </b>" + data.results[0].login.username + " " + data.results[0].login.password;
      document.getElementById('kuva2').src = data.results[0].picture.large;
      document.getElementById('kuva2').style.width = '400px';
   } );
